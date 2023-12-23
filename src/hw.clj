5610bac53943f446416ca219a9ecac7502631b47
Alexander Olshansky <sander2411ukr@gmail.com>
2023-12-23 23:29:35 +0100

    request with params

--- src/hw.clj
+++ src/hw.clj
@@ -1,20 +1,58 @@
 (ns hw
   (:require [io.pedestal.http :as http]
-            [io.pedestal.http.route :as route]))
+            [io.pedestal.http.route :as route]
+            [clojure.string :as s]))
 
-(defn respond-hello [_request]
-  {:status 200 :body "Hello, world!"})
+(defn ok [body]
+  {:status 200 :body body})
+
+(defn not-found []
+  {:status 404 :body "Not found\n"})
+
+(def unmentionables #{"YHWH"
+                      "Voldemort"
+                      "Mxyzptlk"
+                      "Rumplestiltskin"
+                      "曹操"})
+
+
+(defn greeting-for [nm]
+  (cond
+    (unmentionables (s/capitalize nm)) nil
+    (empty? nm) "Hello, world!\n"
+    :else (str "Hello, " nm "\n")))
+
+(defn respond-hello [request]
+  (let [nm (get-in request [:query-params :name])
+        body (greeting-for nm)]
+    (if body
+      (ok body)
+      (not-found))))
 
 
 (def routes
   (route/expand-routes
     #{["/greet" :get respond-hello :route-name :greet]}))
 
-(defn create-server []
-  (http/create-server
-    {::http/routes routes
-     ::http/type :jetty
-     ::http/port 8890}))
+(def service-map
+  {::http/routes routes
+   ::http/type :jetty
+   ::http/port 8890})
 
 (defn start []
-  (http/start (create-server)))
+  (-> service-map http/create-server http/start))
+
+;; For interactive development
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server (-> (assoc service-map ::http/join? false)
+                     http/create-server
+                     http/start)))
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+ 	(start-dev))
